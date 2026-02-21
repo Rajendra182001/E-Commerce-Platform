d5ab3fb1f7acc4524df31ee2bb0425db2652fffa
Rajendra182001 <rajendra.xworkz@gmail.com>
2026-02-21 22:19:58 +0530

    Save local changes before pull

--- springboot-backend/src/main/java/com/xworkz/springboot_backend/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/com/xworkz/springboot_backend/SpringbootBackendApplication.java
@@ -2,12 +2,14 @@ package com.xworkz.springboot_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class 	SpringbootBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBackendApplication.class, args);
+	ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(SpringbootBackendApplication.class, args);
+		System.out.println(configurableApplicationContext.getBeanDefinitionCount());
 	}
 
 }
--- springboot-backend/src/main/java/com/xworkz/springboot_backend/exception/ProductNotFoundException.java
+++ springboot-backend/src/main/java/com/xworkz/springboot_backend/exception/ProductNotFoundException.java
@@ -3,6 +3,7 @@ package com.xworkz.springboot_backend.exception;
 public class ProductNotFoundException extends RuntimeException{
 
     public  ProductNotFoundException(String message){
+
         super(message);
     }
 }
